2cad9e99e20dc7a7722c6dd30967f5dbbb9c384b
unknown <TshepoMagagulaTp11@gmail.com>
2023-08-18 11:15:38 +0200

    final commit

--- SQL-joins.sql
+++ SQL-joins.sql
@@ -18,18 +18,6 @@ JOIN playlist_track ON tracks.trackId = playlist_track.TrackId
 JOIN playlists ON playlists.PlaylistId = playlist_track.PlaylistId
 WHERE playlists.Name = 'Classical'
 
-/* 
-
-
-
-
-
-
-
-
- */
-
-
 -- * :one: Find all the artists for a given Genre
 SELECT DISTINCT a.Name FROM artists a
 JOIN albums b ON a.ArtistId = b.ArtistId
@@ -38,15 +26,34 @@ JOIN  genres d ON c.GenreId = d.GenreId
 WHERE d.Name = 'Jazz'
 
 -- *  :two: Find the Playlist with the most / least songs
-SELECT COUNT(*), MAX(*) AS 'Playlist with the most songs'
+SELECT a.Name, COUNT(*) AS SongCount
 FROM playlists a
 JOIN playlist_track b ON a.PlaylistId = b.PlaylistId
-JOIN tracks c ON b.TrackId = c.TrackId
+GROUP BY a.Name
+ORDER BY SongCount DESC
+LIMIT 1;
 
 -- *  :three: Find the total for a given invoice
+SELECT * FROM invoices
+WHERE invoices.InvoiceId = 17;
 
 -- *  :four: Find all the playlists containing a given genre
+SELECT DISTINCT playlists.Name AS PlayListName FROM playlists
+JOIN playlist_track ON playlists.PlaylistId = playlist_track.PlaylistId
+JOIN tracks ON playlist_track.TrackId = tracks.TrackId
+JOIN genres ON tracks.GenreId = genres.GenreId
+WHERE genres.Name = 'Rock'
 
 -- *  :five: Find the biggest/smallest invoice amounts
+SELECT InvoiceId, MAX(Total) FROM invoices
+SELECT InvoiceId, MIN(Total) FROM invoices
 
 --*  :six: Find the artist with the most/least songs
+SELECT a.Name, COUNT(*) AS TrackCount
+FROM artists a
+JOIN albums b ON a.ArtistId = b.ArtistId
+JOIN tracks c ON b.AlbumId = c.AlbumId
+JOIN  genres d ON c.GenreId = d.GenreId
+GROUP BY a.Name
+ORDER BY TrackCount DESC
+LIMIT 1;
